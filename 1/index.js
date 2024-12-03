cc092f2fd28dbbac5f818a26337610d0d0e03220
Scott Powell <lfh.scot@gmail.com>
2024-12-03 15:52:14 -0500

    day 2 and cleanup

--- 1/index.js
+++ 1/index.js
@@ -8,9 +8,29 @@ const data = require('./data.json');
     const one = dedeuped1[i];
     const two = dedeuped2[i];
     const difference = two - one;
-    const toAdd = (difference < 0 ? Math.abs(difference) : difference)
-    console.log(one, two)
-    total += toAdd;
+    total += Math.abs(difference);
   }
-  console.log(total);
+  console.log('Day 1 Part 1: ', total);
+})();
+
+console.log('-----------------------');
+
+(() => {
+  let total = 0;
+  let frequencyMap = data.list2.reduce((acc, cur) => {
+    if (cur.toString() in acc) {
+      acc[cur] = acc[cur] += 1
+    } else {
+      acc[cur] = 1;
+    }
+    return acc;
+  }, /** @type {Record<string, number>} */({}));
+
+  for (const num of data.list1) {
+    if (num.toString() in frequencyMap) {
+      total += frequencyMap[num] * num;
+    }
+  }
+
+  console.log('Day 1 Part 2: ', total);
 })();

d104638b432c22d076a83cc8a84b944f2312444b
Scott Powell <lfh.scot@gmail.com>
2024-12-03 20:18:08 -0500

    simplify day 2, part 1

--- 2/index.js
+++ 2/index.js
@@ -5,38 +5,27 @@ const data = require('./data.json');
 
   data.forEach(row => {
     const numbers = row.split(' ').map(parseFloat);
-    console.log(numbers)
+    
+
     /** @type {'up' | 'down'} */
-    let direction;
-    let safe = true;
+    let direction = numbers[0] > numbers.slice(1).reduce((a, n, _, ns) => a + n/ns.length, 0) ? 'down' : 'up';
+    let problemIndices = [];
+
     for (let i = 0; i < numbers.length; i++) {
       const number = numbers[i];
       if (i === 0) continue;
-
-      // for the second number, set the direction
-      if (i === 1) {
-        if (number === numbers[0]) {
-          safe = false;
-          break;
-        }
-        if (number > numbers[0]) {
-          direction = 'up';
-        } else {
-          direction = 'down';
-        }
-      }
 
       const difference = direction === 'up' 
         ? number - numbers[i - 1]  // ascending
         : numbers[i - 1] - number; // descending
       
       if (difference < 1 || difference > 3) {
+        problemIndices.push(i);
         safe = false;
-        break;
       }
     }
 
-    if (safe) total += 1;
+    if (!problemIndices.length) total += 1;
   })
 
   console.log('Day 1 Part 1: ', total);
